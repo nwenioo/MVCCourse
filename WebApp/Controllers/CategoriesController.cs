717ff23d5ecca3bb7e3580b55494904cbd9513cd
nwenioo <nwenioo@example.com>
2024-03-02 09:36:48 +0630

    Pass parameters to Action Methods

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id.HasValue)
+                 return new ContentResult { Content = id.ToString() };
+            else
+                return new ContentResult { Content = "null content" };
+        }
+    }
+}
--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace WebApp.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
       
     }
