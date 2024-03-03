a15d10c1a678bf719d7750e445eecf31804e20aa
nwenioo <nwenioo@example.com>
2024-03-03 13:53:44 +0630

    Add Category

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -10,7 +10,6 @@ namespace WebApp.Controllers
             var categories=CategoriesRepository.GetCategories();
             return View(categories);
         }
-        [HttpGet]
         public IActionResult Edit([FromRoute]int? id)
         {
             var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
@@ -22,6 +21,20 @@ namespace WebApp.Controllers
             if (ModelState.IsValid)
             {
                 CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
